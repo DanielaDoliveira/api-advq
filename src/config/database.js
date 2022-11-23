f35e69e1f644ed59e7dae3ded1c2859461ec0127
Daniela Fialho D Oliveira <danielafialhodoliveira@iMac-de-Daniela.local>
2022-11-23 13:03:25 -0300

    Segundo commit

--- src/config/database.js
+++ src/config/database.js
@@ -9,6 +9,3 @@ module.exports = {
     underscored: true,
   },
 };
-//username antigo eraq postgres
-//postgres://USER:PASSWORD@EXTERNAL_HOST:PORT/DATABASE
-//postgres://root:NYCvpOd95YHjLcjPMxBD78GTe9h3ORxe@dpg-cdujotta49967v5r3770-a.oregon-postgres.render.com/projeto_integrado_80vr
