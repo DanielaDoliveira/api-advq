066dee3c6f823cf591cbccd620250b119b1c2441
Daniela Fialho D Oliveira <danielafialhodoliveira@iMac-de-Daniela.local>
2022-11-23 15:47:09 -0300

    Mudando porta

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const cors = require("cors");
 const routes = require("./routes");
 require("./database");
 const app = express();
-
+const port = process.env.PORT || 3333;
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(port);
